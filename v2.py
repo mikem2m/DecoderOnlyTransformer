7c39f9f5eb8f585396d7d34ff55e408d54d5e4eb
mikem2m <michael08ace@gmail.com>
2024-11-13 09:25:29 -0800

    Layer Normalization

--- v2.py
+++ v2.py
@@ -91,12 +91,12 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-        # self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
         # self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        # out = self.dropout(self.proj(out))
+        out = self.dropout(self.proj(out))
         return out
 
 class FeedFoward(nn.Module):
